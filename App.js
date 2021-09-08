2be5cddb9e576d44417cef85d046bed772b5ad69
Thibault Friedrich <thibault.friedrich@gmail.com>
2021-09-07 22:12:54 -0400

    feat: add jost font

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.scan}>
+        Open up App.js to start working on your app!
+      </Text>
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#16161a',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  scan: {
+    fontSize: 25,
+    color: 'white',
+    textAlign: 'center',
+    fontFamily: 'Jost-Medium',
+  },
+})
---
+++ README.md
@@ -0,0 +1,9 @@
+# Vaxicode-redesign
+
+This project is a redesign of the Vaxicode application, the Québec application to manage vaccinal passports.
+
+```bash
+yarn
+yarn start
+
+```
