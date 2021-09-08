899598d1f75b45e11d368e9686e49d76eb8e8b03
Thibault Friedrich <thibault.friedrich@gmail.com>
2021-09-08 19:20:51 -0400

    feat: add confirmation popup to save

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 
-export default ({ ...props }) => <Button style={styles.button} {...props} />
+export default ({ title, onPress }) => (
+  <TouchableOpacity style={styles.button}>
+    <Text style={styles.title}>{title}</Text>
+  </TouchableOpacity>
+)
 
 const styles = StyleSheet.create({
   button: {
     backgroundColor: '#7f5af0',
+    padding: 10,
+    borderRadius: 5,
+  },
+  title: {
     color: '#fffffe',
+    fontSize: 23,
+    textAlign: 'center',
+    fontFamily: 'Jost-Medium',
   },
 })
--- src/components/pages/Scan.js
+++ src/components/pages/Scan.js
@@ -5,13 +5,17 @@ import { Camera } from 'expo-camera'
 import { Icon } from 'react-native-elements'
 import { BarCodeScanner } from 'expo-barcode-scanner'
 import QRCode from 'react-native-qrcode-svg'
+import RBSheet from 'react-native-raw-bottom-sheet'
 
 import parseShc from '../../utils/parseShc'
+import Button from '../atoms/Button'
 
 export default () => {
   const [hasPermission, setHasPermission] = useState(null)
   const [type, setType] = useState(Camera.Constants.Type.back)
   const barCodeScanned = useRef('')
+
+  const bottomPanel = useRef()
 
   const [proof, setProof] = useState('')
 
@@ -36,6 +40,7 @@ export default () => {
     barCodeScanned.current = data
     const parsedData = parseShc(data)
     setProof({ raw: data, ...parsedData })
+    bottomPanel.current.open()
     // Alert.alert('Scanned', JSON.stringify(payload), [
     //   {
     //     text: 'Cancel',
@@ -46,6 +51,11 @@ export default () => {
     // ])
   }
 
+  const onBottomPanelClose = () => {
+    barCodeScanned.current = ''
+    setProof('')
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.scan}>
@@ -53,15 +63,18 @@ export default () => {
       </Text>
 
       <View style={styles.cameraContainer}>
-        <Camera
-          style={styles.camera}
-          type={type}
-          barCodeScannerSettings={{
-            barCodeTypes: [BarCodeScanner.Constants.BarCodeType.qr],
-          }}
-          onBarCodeScanned={scanned}
-        >
-          {/* <Icon
+        {proof ? (
+          <QRCode value={proof.raw} size={300} logoSize={300} />
+        ) : (
+          <Camera
+            style={styles.camera}
+            type={type}
+            barCodeScannerSettings={{
+              barCodeTypes: [BarCodeScanner.Constants.BarCodeType.qr],
+            }}
+            onBarCodeScanned={scanned}
+          >
+            {/* <Icon
             name='qr-code-outline'
             style={styles.scanIcon}
             color='white'
@@ -69,9 +82,24 @@ export default () => {
             size={300}
             iconStyle={styles.scanIcon}
           /> */}
-        </Camera>
+          </Camera>
+        )}
       </View>
-      {proof ? <QRCode value={proof.raw} /> : null}
+      <RBSheet
+        ref={bottomPanel}
+        height={300}
+        openDuration={250}
+        onClose={onBottomPanelClose}
+        customStyles={{ container: styles.bottomPanel }}
+      >
+        <View style={styles.bottomPanelContent}>
+          <Text style={styles.bottomPanelTitle}>
+            Vaccination proof detected
+          </Text>
+          <Text style={styles.bottomPanelName}>Thibault Friedrich</Text>
+        </View>
+        <Button title='Save' />
+      </RBSheet>
     </View>
   )
 }
@@ -106,5 +134,27 @@ const styles = StyleSheet.create({
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bottomPanel: {
+    padding: 20,
+    backgroundColor: '#16161a',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  bottomPanelContent: {
+    flex: 1,
+  },
+  bottomPanelTitle: {
+    fontSize: 18,
+    color: '#94a1b2',
+    textAlign: 'center',
+    fontFamily: 'Jost-Medium',
+  },
+  bottomPanelName: {
+    fontSize: 35,
+    color: '#fffffe',
+    textAlign: 'center',
+    fontFamily: 'Jost-Medium',
+    paddingTop: 20,
   },
 })
